4f30f552eb547744558bc1c71da8920636382506
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2023-11-03 13:44:24 +0000

    main

--- server/index.js
+++ server/index.js
@@ -68,4 +68,7 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => console.log("Server running at port 5000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+   console.log(`🎯 Server is running on PORT: ${PORT}`);
+});
