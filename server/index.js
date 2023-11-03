77de9266eec0c6e2235610c28b4cfd3685029204
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2023-11-03 16:54:59 +0000

    main

--- server/index.js
+++ server/index.js
@@ -13,7 +13,19 @@ admin.initializeApp({
 });
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = ["https://chat-app-psi-flame.vercel.app"]; // Add your domain here
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 const server = http.createServer(app);
 const io = socketio(server);
