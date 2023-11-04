a5f996a3c0d4e4e32b591416db596522174f7b51
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2023-11-04 04:52:02 +0000

    main

--- server/index.js
+++ server/index.js
@@ -2,37 +2,35 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const socketio = require('socket.io');
-const admin = require("firebase-admin"); // Import Firebase Admin SDK
+const socketio = require("socket.io");
+const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK with your service account key
 const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://mychat-b309a-default-rtdb.firebaseio.com", // Replace with your Firebase Realtime Database URL
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mychat-b309a-default-rtdb.firebaseio.com",
 });
+
 const PORT = process.env.PORT || 5000;
 
 // Define your CORS options
 // Use the cors middleware to allow requests from your frontend app
-app.use(cors({
-    origin: 'https://chat-app-psi-flame.vercel.app', // Replace with your frontend app's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization', // Add any additional headers as needed
-  }));
-  
-app.use((req, res, next) => {
-    // Set custom headers for Mozilla Firefox
-    res.setHeader('Access-Control-Allow-Origin', 'https://chat-app-psi-flame.vercel.app'); // Replace with your frontend app's URL
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  }); 
+app.use(
+  cors({
+    origin: "https://chat-app-psi-flame.vercel.app", // Replace with your frontend app's URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization", // Add any additional headers as needed
+  })
+);
 
 const server = http.createServer(app);
 const io = socketio(server);
 
+
+
+let connectedUsers = 0;
+const maxUsers = 144; // Set the maximum number of users
 io.on("connection", (socket) => {
     const transport = socket.conn.transport.name; // in most cases, "polling"
   
@@ -40,45 +38,41 @@ io.on("connection", (socket) => {
       const upgradedTransport = socket.conn.transport.name; // in most cases, "websocket"
     });
   });
-  
-let connectedUsers = 0;
-const maxUsers = 144; // Set the maximum number of users
-
 io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+  console.log(`User connected: ${socket.id}`);
 
-    if (connectedUsers < maxUsers) {
-        connectedUsers++;
-        io.emit("user-joined", `User ${connectedUsers} has joined.`);
-    } else {
-        // Send a message to the client that the limit is reached
-        socket.emit("limit-reached", "Chat room is full. The limit is 4 users.");
-        socket.disconnect();
-    }
+  if (connectedUsers < maxUsers) {
+    connectedUsers++;
+    io.emit("user-joined", `User ${connectedUsers} has joined.`);
+  } else {
+    // Send a message to the client that the limit is reached
+    socket.emit("limit-reached", "Chat room is full. The limit is 4 users.");
+    socket.disconnect();
+  }
 
-    socket.on("send-message", (message) => {
-        console.log(message);
-        const messagesRef = admin.database().ref("messages");
-        const newMessageRef = messagesRef.push();
-        newMessageRef.set({
-            user: message.user,
-            message: message.message,
-            time: message.time,
-        });
-
-        io.emit("received-message", message);
+  socket.on("send-message", (message) => {
+    console.log(message);
+    const messagesRef = admin.database().ref("messages");
+    const newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      user: message.user,
+      message: message.message,
+      time: message.time,
     });
 
-    socket.on("disconnect", () => {
-        console.log(`User disconnected: ${socket.id}`);
-        connectedUsers--;
-    });
+    io.emit("received-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+    connectedUsers--;
+  });
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, World</h1>');
-  });
-  
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Yash</h1>");
+});
+
 server.listen(PORT, () => {
-   console.log(`🎯 Server is running on PORT: ${PORT}`);
+  console.log(`🎯 Server is running on PORT: ${PORT}`);
 });
